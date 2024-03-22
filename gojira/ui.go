1c1abab92308bb272df3da48dc14b1fe78cd0edf
Jarosław Zywert <j.zywert@piwik.pro>
2024-03-22 11:55:29 +0100

    grid instead flex for more controlable layout

--- gojira/ui.go
+++ gojira/ui.go
@@ -36,16 +36,16 @@ func newUi() {
 
 	grid := tview.NewGrid().
 		SetRows(2, 0, 2).
-		SetColumns(0, 28).
+		SetColumns(0, 27).
 		SetBorders(true)
-
+	grid.SetTitle(" gojira ")
 	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
-	grid.AddItem(app.ui.pages, 1, 0, 1, 2, 0, 0, false)
+	//grid.AddItem(app.ui.pages, 1, 0, 1, 2, 0, 0, false)
 
 	// Layout for screens wider than 100 cells.
-	grid.AddItem(app.ui.pages, 1, 0, 1, 1, 0, 100, false).
-		AddItem(app.ui.calendar, 1, 1, 1, 1, 0, 100, false)
+	grid.AddItem(app.ui.pages, 1, 0, 1, 1, 0, 0, false).
+		AddItem(app.ui.calendar, 1, 1, 1, 1, 0, 0, false)
 
 	app.ui.flex.SetBorder(true).SetTitle(" gojira ")
-	app.ui.app.SetRoot(grid, true)
+	app.ui.app.SetRoot(grid, true).SetFocus(app.ui.pages)
 }
