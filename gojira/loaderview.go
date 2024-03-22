83ed08b7f7d031a18e205167e8ff9440e3571602
Jarosław Zywert <j.zywert@piwik.pro>
2024-03-22 15:58:46 +0100

    example custom modal - form seems to be untouchable

--- gojira/loaderview.go
+++ gojira/loaderview.go
@@ -3,29 +3,20 @@ package gojira
 import (
 	"context"
 	"fmt"
-	"github.com/rivo/tview"
+	"github.com/gdamore/tcell/v2"
 	"time"
 )
 
 type LoaderView struct {
-	tview.Primitive
-	text   *tview.TextView
+	*Modal
 	ctx    context.Context
 	cancel context.CancelFunc
 }
 
 func NewLoaderView() *LoaderView {
-	customModal := func(p tview.Primitive, width, height int) tview.Primitive {
-		return tview.NewGrid().
-			SetColumns(0, width, 0).
-			SetRows(0, height, 0).
-			AddItem(p, 1, 1, 1, 1, 0, 0, true)
-	}
-	text := tview.NewTextView()
-	text.SetBorder(true)
-	text.SetTextAlign(tview.AlignCenter)
-
-	loaderView := &LoaderView{customModal(text, 36, 13), text, nil, nil}
+	loaderView := &LoaderView{NewModal(), nil, nil}
+	loaderView.SetBorder(false)
+	loaderView.SetBackgroundColor(tcell.ColorBlueViolet.TrueColor())
 	app.ui.pages.AddPage("loader", loaderView, true, false)
 	return loaderView
 }
@@ -49,7 +40,7 @@ func (e *LoaderView) Show(msg string) {
 				return
 			default:
 				for _, r := range `-\|/` {
-					e.text.SetText(fmt.Sprintf("%s%s\n%s", GojiraAscii, msg, string(r)))
+					e.SetText(fmt.Sprintf("%s%s\n%s", GojiraAscii, msg, string(r)))
 					app.ui.app.Draw()
 					time.Sleep(100 * time.Millisecond)
 				}
