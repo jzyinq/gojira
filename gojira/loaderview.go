fbd782fac5cd0ed8d0affe6bd3cd9818bf0facab
Jarosław Zywert <j.zywert@piwik.pro>
2024-03-22 15:17:54 +0100

    restore grid with non-center loader

--- gojira/loaderview.go
+++ gojira/loaderview.go
@@ -8,17 +8,25 @@ import (
 )
 
 type LoaderView struct {
-	*tview.TextView
+	tview.Primitive
+	text   *tview.TextView
 	ctx    context.Context
 	cancel context.CancelFunc
 }
 
 func NewLoaderView() *LoaderView {
+	customModal := func(p tview.Primitive, width, height int) tview.Primitive {
+		return tview.NewGrid().
+			SetColumns(0, width, 0).
+			SetRows(0, height, 0).
+			AddItem(p, 1, 1, 1, 1, 0, 0, true)
+	}
 	text := tview.NewTextView()
 	text.SetBorder(true)
 	text.SetTextAlign(tview.AlignCenter)
 
-	loaderView := &LoaderView{text, nil, nil}
+	loaderView := &LoaderView{customModal(text, 36, 13), text, nil, nil}
+	app.ui.pages.AddPage("loader", loaderView, true, false)
 	return loaderView
 }
 
@@ -41,7 +49,7 @@ func (e *LoaderView) Show(msg string) {
 				return
 			default:
 				for _, r := range `-\|/` {
-					e.SetText(fmt.Sprintf("%s%s\n%s", GojiraAscii, msg, string(r)))
+					e.text.SetText(fmt.Sprintf("%s%s\n%s", GojiraAscii, msg, string(r)))
 					app.ui.app.Draw()
 					time.Sleep(100 * time.Millisecond)
 				}
